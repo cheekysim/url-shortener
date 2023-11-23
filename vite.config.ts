f32cec1824c7abca9bcbaf11088ba2bd87737368
cheekysim <minecraftcheekysim@gmail.com>
2023-11-23 00:23:26 +0000

    Removed unused proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,6 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		port: 4006,
-		strictPort: true,
-		proxy: {
-			'/api': {
-				target: 'http://localhost:4005/api/v1',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/\/api/, ''),
-				configure: (proxy) => {
-					proxy.on('error', (err) => {
-						console.log('proxy error', err);
-					});
-					proxy.on('proxyReq', (proxyReq, req) => {
-						console.log('Sending Request to the Target:', req.method, req.url);
-					});
-					proxy.on('proxyRes', (proxyRes, req) => {
-						console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-					});
-				}
-			}
-		}
+		strictPort: true
 	}
 });
